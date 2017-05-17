1b72210ebd216e1eedbe7b289179e060c3d3e99a
Soufiane  Ghzal <sghzal@gmail.com>
2017-05-17 20:16:01 +0200

    better styles

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var fs = require("fs");
 var Handlebars = require("handlebars");
 
+Handlebars.registerHelper('dateYear', function(date){
+	return date.substring(0, 4);
+});
+
 function render(resume) {
 	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
 	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
